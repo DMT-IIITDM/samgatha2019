10ff1ce3dd6811d0e625a2f8e768b20b98cdc8ab
Yggdrasil09 <karthik99th.tnk@gmail.com>
2019-02-08 14:10:15 +0530

    hamburger nav

--- js/overlay.js
+++ js/overlay.js
@@ -33,18 +33,21 @@ function menu() {
         $('#close').toggleClass('active');
         $('.nav-item').toggleClass('active');
         $('#social').toggleClass('active');
+        list4[0][1]=0;
     });
     $('#close1').on('click', function () {
         $('#overlay1').toggleClass('active');
         $('#close1').toggleClass('active');
         $('.nav-item').toggleClass('active');
         $('#social').toggleClass('active');
+        list4[1][1]=0;
     });
     $('#close2').on('click', function () {
         $('#overlay2').toggleClass('active');
         $('#close2').toggleClass('active');
         $('.nav-item').toggleClass('active');
         $('#social').toggleClass('active');
+        list4[2][1]=0;
     });
     // DANCE
     $('#dance1').on('click', function () {
@@ -73,18 +76,21 @@ function menu() {
         $('#close3').toggleClass('active');
         $('.nav-item').toggleClass('active');
         $('#social').toggleClass('active');
+        list4[3][1]=0;
     });
     $('#close4').on('click', function () {
         $('#overlay4').toggleClass('active');
         $('#close4').toggleClass('active');
         $('.nav-item').toggleClass('active');
         $('#social').toggleClass('active');
+        list4[4][1]=0;
     });
     $('#close5').on('click', function () {
         $('#overlay5').toggleClass('active');
         $('#close5').toggleClass('active');
         $('.nav-item').toggleClass('active');
         $('#social').toggleClass('active');
+        list4[5][1]=0;
     });
     // ART
     $('#art1').on('click', function () {
@@ -113,18 +119,21 @@ function menu() {
         $('#close6').toggleClass('active');
         $('.nav-item').toggleClass('active');
         $('#social').toggleClass('active');
+        list4[6][1]=0;
     });
     $('#close7').on('click', function () {
         $('#overlay7').toggleClass('active');
         $('#close7').toggleClass('active');
         $('.nav-item').toggleClass('active');
         $('#social').toggleClass('active');
+        list4[7][1]=0;
     });
     $('#close8').on('click', function () {
         $('#overlay8').toggleClass('active');
         $('#close8').toggleClass('active');
         $('.nav-item').toggleClass('active');
         $('#social').toggleClass('active');
+        list4[8][1]=0;
     });
     // MUSIC
     $('#music1').on('click', function () {
@@ -146,12 +155,14 @@ function menu() {
         $('#close9').toggleClass('active');
         $('.nav-item').toggleClass('active');
         $('#social').toggleClass('active');
+        list4[9][1]=0;
     });
     $('#close10').on('click', function () {
         $('#overlay10').toggleClass('active');
         $('#close10').toggleClass('active');
         $('.nav-item').toggleClass('active');
         $('#social').toggleClass('active');
+        list4[10][1]=0;
     });
 
     //Gaming
@@ -181,18 +192,21 @@ function menu() {
         $('#close11').toggleClass('active');
         $('.nav-item').toggleClass('active');
         $('#social').toggleClass('active');
+        list4[11][1]=0;
     });
     $('#close12').on('click', function () {
         $('#overlay12').toggleClass('active');
         $('#close12').toggleClass('active');
         $('.nav-item').toggleClass('active');
         $('#social').toggleClass('active');
+        list4[12][1]=0;
     });
     $('#close13').on('click', function () {
         $('#overlay13').toggleClass('active');
         $('#close13').toggleClass('active');
         $('.nav-item').toggleClass('active');
         $('#social').toggleClass('active');
+        list4[13][1]=0;
     });
     //Drama-Spur of the moment
     $('#drama17').on('click', function () {
@@ -221,18 +235,21 @@ function menu() {
         $('#close17').toggleClass('active');
         $('.nav-item').toggleClass('active');
         $('#social').toggleClass('active');
+        list4[14][1]=0;
     });
     $('#close18').on('click', function () {
         $('#overlay18').toggleClass('active');
         $('#close18').toggleClass('active');
         $('.nav-item').toggleClass('active');
         $('#social').toggleClass('active');
+        list4[15][1]=0;
     });
     $('#close19').on('click', function () {
         $('#overlay19').toggleClass('active');
         $('#close19').toggleClass('active');
         $('.nav-item').toggleClass('active');
         $('#social').toggleClass('active');
+        list4[16][1]=0;
     });
     //Quiz
     $('#quiz20').on('click', function () {
@@ -261,17 +278,20 @@ function menu() {
         $('#close20').toggleClass('active');
         $('.nav-item').toggleClass('active');
         $('#social').toggleClass('active');
+        list4[17][1]=0;
     });
     $('#close21').on('click', function () {
         $('#overlay21').toggleClass('active');
         $('#close21').toggleClass('active');
         $('.nav-item').toggleClass('active');
         $('#social').toggleClass('active');
+        list4[18][1]=0;
     });
     $('#close22').on('click', function () {
         $('#overlay22').toggleClass('active');
         $('#close22').toggleClass('active');
         $('.nav-item').toggleClass('active');
         $('#social').toggleClass('active');
+        list4[19][1]=0;
     });
 }
